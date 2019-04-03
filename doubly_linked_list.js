d1ce74e6c361bb8e48403dc99790fdbafa99393a
Mike <miketu926@gmail.com>
2019-04-03 17:22:40 -0400

    Added dynamic programming

---
+++ doubly_linked_list.js
@@ -0,0 +1,36 @@
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+
+  setHead(data) {
+
+  }
+
+  setTail(data) {
+
+  }
+
+  insertBefore(node, data) {}
+
+  insertAfter(node, data) {}
+
+  insertAtPosition(position, data) {}
+
+  remove(data) {}
+
+  findNodeWithData(data) {}
+
+
+
+
+}
