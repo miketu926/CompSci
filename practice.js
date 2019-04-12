e18aaeaf51b8af2b8be0564dcfff1cc5c1c05e28
Mike <miketu926@gmail.com>
2019-04-12 14:22:22 -0400

    Added quickSort and binarySearch to practice

--- practice.js
+++ practice.js
@@ -1,3 +1,12 @@
+// problem set
+// how find works (takes a function)
+
+// flatten
+// bubbleSort
+// mergeSort
+// quickSort
+// binarySearch
+
 a = [1, 2, 3, 4, 5];
 
 let checkage = age => {
@@ -39,11 +48,6 @@ const bubbleSort = arr => {
       let j = i + 1;
 
       if (arr[i] > arr[j]) {
-        // let temp = arr[i];
-        // arr[i] = arr[j];
-        // arr[j] = temp;
-
-        // another way to swap using ES6 -
         [arr[i], arr[j]] = [arr[j], arr[i]];
         finished = false;
       }
@@ -52,6 +56,80 @@ const bubbleSort = arr => {
   return arr;
 };
 
-console.log(bubbleSort([3, 9, 4, 6, 3]));
+// bubbleSort test:
+// console.log(bubbleSort([3, 9, 4, 6, 3]));
 
-// new commit
+const mergeSort = (arr) => {
+  if (arr.length <= 1) {
+    return arr;
+  }
+
+  let midIdx = Math.floor(arr.length / 2);
+  let left = arr.slice(0, midIdx);
+  let right = arr.slice(midIdx);
+
+  let leftSorted = mergeSort(left);
+  let rightSorted = mergeSort(right);
+
+  return merge(leftSorted, rightSorted);
+
+};
+
+const merge = (arr1, arr2) => {
+  let merged = [];
+
+  while (arr1.length && arr2.length) {
+    if (arr1[0] < arr2[0]) {
+      merged.push(arr1.shift());
+    } else {
+      merged.push(arr2.shift());
+    }
+  }
+
+  return [...merged, ...arr1, ...arr2];
+};
+
+
+// mergeSort test:
+// console.log(mergeSort([3, 5, 2, 1, 0, 9]));
+
+const quickSort = arr => {
+  if (arr.length <= 1) {
+    return arr;
+  }
+
+  // shift takes out the element of the array
+  // so that it can reach the base case
+  let pivot = arr.shift();
+  let left = arr.filter(el => el < pivot);
+  let right = arr.filter(el => el >= pivot);
+
+  let leftSorted = quickSort(left);
+  let rightSorted = quickSort(right);
+
+  return [...leftSorted, pivot, ...rightSorted];
+
+};
+
+// console.log(quickSort([3, 5, 2, 1, 0, 9]));
+
+const binarySearch = (arr, target) => {
+  if (arr.length <= 0) {
+    return false;
+  }
+
+  let midIdx = Math.floor(arr.length / 2);
+  let left = arr.slice(0, midIdx);
+  let right = arr.slice(midIdx + 1);
+
+  if (target < arr[midIdx]) {
+    return binarySearch(left, target);
+  } else if (target > arr[midIdx]) {
+    return binarySearch(right, target);
+  } else {
+    return true;
+  }
+};
+
+console.log(binarySearch([-1, 0, 5, 7, 10, 19], 19)); // true
+console.log(binarySearch([-1, 0, 5, 7, 10, 19], 1)); // false
