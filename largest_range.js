17adc8047f11d23249004faf9b1220ad64365638
Mike <miketu926@gmail.com>
2019-07-16 14:50:44 -0400

    completed largest range

--- largest_range.js
+++ largest_range.js
@@ -1,7 +1,7 @@
 // Write a function that takes in an array of integers and returns an array of length 2 represeting
 // the largest range of numbers contained in that array. 
-// The first number in the output array should be the first number in teh range while the second number
-// should be the last number in teh range.
+// The first number in the output array should be the first number in the range while the second number
+// should be the last number in the range.
 
 // a range is: [1,5] = {1,2,3,4,5} where range = 5
 
@@ -9,35 +9,35 @@
 // output: [0,7]
 
 const largestRange = (arr) => {
-  arr.sort((a, b) => a - b);
+  arr.sort((a, b) => a - b)
 
   // then have one pointer at first idx, then another to see if +1 gives the next
-  // number. if not, reset and start
+  // number. if not, reset and go
   // [0,1,2,3,4,5,6,7,10,11,12,15]
-  if (arr.length === 1) return [arr[0], arr[0]];
 
-  let range = 1;
-  let start = arr[0];
-  let end = arr[1];
-  let maxRange = end - start + 1;
-  let result = [start, start + maxRange - 1];
+  let currResult = [arr[0], arr[0]];
+  let result = currResult;
+  if (arr.length === 1) return result;
+  if (arr.length === 2) return [arr[0], arr[1]];
 
-  for (let i = 0; i < arr.length - 1; i++) {
-    if (arr[i] + 1 === arr[i + 1] || arr[i] === arr[i + 1]) {
-      range++;
-      end = arr[i + 1];
-      continue;
+  console.log(arr);
+  console.log(currResult);
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] - 1 === arr[i - 1] || arr[i] === arr[i - 1]) {
+      currResult[1] = arr[i]
     } else {
-      if (range > maxRange) {
-        result[0] = start;
-        result[1] = start + end - 1;
-        maxRange = range;
-      }
-      start = arr[i];
-      end = arr[i + 1]
-      range = 1;
+      currResult = [arr[i], arr[i]]
+    }
+
+    console.log(currResult);
+
+    if (result[1] - result[0] + 1 < currResult[1] - currResult[0] + 1) {
+      result = currResult;
     }
   }
 
   return result;
 };
+
+console.log(largestRange([0, 1, 2, 3, 4, 5, 6, 7, 10, 11, 12, 15]));
