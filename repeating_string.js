26b6e85fd6a632316af753f029d925dfa0095000
Mike <miketu926@gmail.com>
2019-07-15 23:28:26 -0400

    Updated repeating_string

--- repeating_string.js
+++ repeating_string.js
@@ -35,14 +35,14 @@ const repeatingStr = (str) => {
   // }
 
   for (let i = 0; i < str.length; i++) {
-    obj[str[i]] === undefined ? obj[str[i]] = 1 : obj[str[i]] += 1;
+    !obj[str[i]] ? obj[str[i]] = 1 : obj[str[i]] += 1;
   };
 
-  for (let char in obj) {
+  for (const char in obj) {
     result += char + obj[char];
   }
 
   return result;
 };
 
-console.log(repeatingStr('aaaabbAAcc'));
+console.log(repeatingStr('aaabbAAcc'));
