1aedbecdcbe69a006b44c3058162459e3b54dee8
Mike <miketu926@gmail.com>
2019-08-17 23:01:14 -0400

    Updated house_robber

--- house_robber.js
+++ house_robber.js
@@ -2,23 +2,34 @@ const houseRobber = (nums) => {
   if (nums.length === 0) return 0;
   if (nums.length === 1) return nums[0];
 
-  // const result = Array(nums.length);
-  // result[0] = nums[0]
-  // result[1] = Math.max(nums[0], nums[1]);
+  const result = Array(nums.length);
+  result[0] = nums[0]
+  result[1] = Math.max(nums[0], nums[1]);
+
+  for (let i = 2; i < nums.length; i++) {
+    result[i] = Math.max(nums[i] + result[i - 2], result[i - 1])
+  }
+
+  return result[result.length - 1];
+};
+
+console.log(houseRobber([1, 2, 3, 1]));
+
+const houseRobberConstantSpace = (nums) => {
+  if (nums.length === 0) return 0;
+  if (nums.length === 1) return nums[0];
 
   let temp;
   let left = nums[0]
   let right = Math.max(nums[0], nums[1])
 
   for (let i = 2; i < nums.length; i++) {
-    // result[i] = Math.max(nums[i]+result[i-2], result[i-1])
     temp = Math.max(nums[i] + left, right);
     left = right;
     right = temp;
   }
 
-  // return result[result.length-1];
   return right;
 };
 
-console.log(houseRobber([1, 2, 3, 1]));
+console.log(houseRobberConstantSpace([1, 2, 3, 1]));
