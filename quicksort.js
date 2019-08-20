785558e57ab2608563358c3f0bd4acd5b3dd0628
Mike <miketu926@gmail.com>
2019-08-20 16:30:32 -0400

    updated quicksort

--- quicksort.js
+++ quicksort.js
@@ -1,41 +1,32 @@
-(function () {
+const QuickSort = function (inputArr) {
+  let left = [];
+  let right = [];
+  let pivot;
 
-  const QuickSort = function (inputArr) {
+  if (inputArr.length === 0) {
+    return inputArr;
+  }
 
-    let len = inputArr.length,
-      left = [],
-      right = [],
-      pivot;
+  pivot = inputArr[0];
 
-    if (len === 0) {
-      return inputArr;
+  // partition values into upper and lower
+  // start at second element, since we are using first element as the pivot
+  for (let i = 1; i < inputArr.length; i++) {
+
+    if (inputArr[i] <= pivot) {
+      left.push(inputArr[i]);
     }
-
-    pivot = inputArr[0];
-
-    // partition values into upper and lower
-    // start at second element, since we are using first element as the pivot
-    for (let i = 1; i < len; i++) {
-
-      if (inputArr[i] <= pivot) {
-        left.push(inputArr[i]);
-      }
-      else {
-        right.push(inputArr[i]);
-      }
-
+    else {
+      right.push(inputArr[i]);
     }
+  }
 
-    left = QuickSort(left);
-    right = QuickSort(right);
+  left = QuickSort(left);
+  right = QuickSort(right);
 
-    return left.concat(pivot, right);
+  return left.concat(pivot, right);
+};
 
-  };
 
-  const myArray = [2, 4, 1, 6, 8, 5, 9, 3, 4];
-
-  // [1, 2, 3, 4, 4, 5, 6, 8, 9]
-  console.log(QuickSort(myArray));
-
-}());
+const myArray = [2, 4, 1, 6, 8, 5, 9, 3, 4];
+console.log(QuickSort(myArray));
