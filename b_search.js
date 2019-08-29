1f97b664321b37fdf9a374a19f6619daff7ef4ac
Mike <miketu926@gmail.com>
2019-08-29 12:11:13 -0400

    Added b_search

---
+++ b_search.js
@@ -0,0 +1,24 @@
+const binarySearch = (arr, target) => {
+
+  let L = 0;
+  let R = arr.length - 1;
+
+  while (L <= R) {
+    let mid = Math.floor((R + L) / 2);
+
+    if (arr[mid] === target) {
+      return mid;
+    } else if (target > arr[mid]) {
+      L = mid + 1;
+    } else {
+      R = mid - 1;
+    }
+  }
+  return false;
+};
+
+console.log(binarySearch([-1, 0, 5, 7, 10, 19], 19)); // 5
+console.log(binarySearch([-1, 0, 5, 7, 10, 19], -1)); // 0
+console.log(binarySearch([-1, 0, 5, 7, 10, 19], 5)); // 2
+console.log(binarySearch([-1, 0, 5, 7, 10, 19], 0)); // 1
+console.log(binarySearch([-1, 0, 5, 7, 10, 19], 2)); // false
