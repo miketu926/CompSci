59f7e966a90f31967831f05d3ddc245bf91187e8
Mike <miketu926@gmail.com>
2019-10-01 17:15:32 -0400

    Updated stock price 2

--- practice.js
+++ practice.js
@@ -25,24 +25,6 @@ let checkage = age => {
 
 // console.log(a.find(checkage));
 
-
-// 2) Given same arr of stock prices, find the most profit that can be made
-// ex: [5, 6, 4, 2, 8, 7, 1, 2, 9, 2] ans: (6 - 5) + (8 - 2) + (9 - 1) => 8
-
-const stockPrice2 = (prices) => {
-  let max = 0;
-  let i = 0;
-
-  while (i < prices.length - 1) {
-    max += Math.max(0, prices[i + 1] - prices[i]);
-    i++;
-  }
-
-  return max;
-};
-
-// console.log(stockPrice2([5, 6, 4, 2, 8, 7, 1, 2, 9, 2]));
-
 const reverseSentInPlace = (sent) => {
   return sent.split('').reverse().join('');
 };
--- stock_price2.js
+++ stock_price2.js
@@ -12,3 +12,22 @@ const stockPrice2 = (prices) => {
 }
 
 console.log(stockPrice2([5, 6, 4, 2, 8, 7, 1, 2, 9, 2]));  // should output 15 as total profit
+
+
+
+// 2) Given same arr of stock prices, find the most profit that can be made
+// ex: [5, 6, 4, 2, 8, 7, 1, 2, 9, 2] ans: (6 - 5) + (8 - 2) + (9 - 1) => 8
+
+const stockPrice2 = (prices) => {
+  let max = 0;
+  let i = 0;
+
+  while (i < prices.length - 1) {
+    max += Math.max(0, prices[i + 1] - prices[i]);
+    i++;
+  }
+
+  return max;
+};
+
+// console.log(stockPrice2([5, 6, 4, 2, 8, 7, 1, 2, 9, 2]));
