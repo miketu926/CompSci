76e65e270e7f09e71dbededd5eda6f0037d1a02e
Mike <miketu926@gmail.com>
2019-10-07 00:11:40 -0400

    Added .gitignore

---
+++ sevenish.js
@@ -0,0 +1,12 @@
+// Let's define a "sevenish" number to be one which is either a power of 7,
+// or the sum of unique powers of 7. The first few sevenish numbers are 1, 7, 8, 49, and so on.
+// Create an algorithm to find the nth sevenish number.
+
+const sevenish = () => {
+
+}
+
+const powerOfSeven = (n) => {
+  if (n < 7) n = n / 7;
+
+}
