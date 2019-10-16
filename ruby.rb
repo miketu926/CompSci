736ee935f9cc42f223390950a06e894650d70895
Mike <miketu926@gmail.com>
2019-10-16 10:43:49 -0400

    added ruby comments

--- ruby.rb
+++ ruby.rb
@@ -1,13 +1,15 @@
-def some_problem(arr, string = "a")
+def some_problem(arr, string = 'abcd')
   # arr.each { |el| p el } 
   result = arr
-  string = 'abcd'
   
   hash = Hash.new(0)
 
   arr.each_with_index do |el, idx|
-    p el
-    p idx
+    p idx.to_s + " " + el.to_s
+  end
+
+  string.each_char.with_index do |char, idx|
+    p idx.to_s + " " + char
   end
 
   p string
@@ -16,7 +18,5 @@ def some_problem(arr, string = "a")
   p result[1..2]
   return result
 end
-
-
 
 p some_problem([1,2,3])
