a2456281e890ed35505b291d077a9bc3e439cfd4
Mike <miketu926@gmail.com>
2019-11-15 01:03:02 -0500

    Added counting hash

---
+++ counting_hash.js
@@ -0,0 +1,22 @@
+const str = 'bananas'
+
+const counterV1 = (str) => {
+  const counter = {};
+  for (const letter of str) {
+    counter[letter] ? counter[letter]++ : counter[letter] = 1;
+  }
+  return counter;
+}
+
+const counterV2 = (str) => {
+  const counter = {};
+  for (const letter of str) {
+    counter[letter] = (counter[letter] || 0) + 1;
+  }
+  return counter;
+}
+
+
+console.log(counterV1(str));
+console.log(counterV2(str));
+console.log(counterV3(str));
