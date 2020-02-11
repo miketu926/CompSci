299117fd5d59b814ebaec862a5281cfe7df80940
Mike <michael.obsessvr.com>
2020-02-11 15:42:18 -0500

    Updated arr_matrix

--- arr_matrix.js
+++ arr_matrix.js
@@ -1,5 +1,4 @@
-// const words = ['eat', 'rain', 'in', 'rat'];
-const words = ['in'];
+const words = ['eat', 'rain', 'in', 'rat'];
 
 // matrix M*N
 const matrix = [
@@ -18,11 +17,11 @@ const dictMatrix = (words, matrix) => {
   const sortedWordsByLen = words.sort((a, b) => a.length - b.length);
 
   // numOfWords*(M*N)
-  // first word === 'in'
+  // first word is 'in'
   for (const word of sortedWordsByLen) {
     for (let i = 0; i < matrix.length; i++) {
       for (let j = 0; j < matrix[0].length; j++) {
-        if (word[0] === matrix[i][j] && dfs(word, i, j, matrix, 0)) {
+        if (word[0] === matrix[i][j] && _dfs(word, i, j, matrix, 0)) {
           // pass word, i, j, matrix, idx => add if word is complete
           result += 1;
         }
@@ -33,24 +32,21 @@ const dictMatrix = (words, matrix) => {
   return result;
 };
 
-const dfs = (word, i, j, matrix, count) => {
-  console.log(word);
+const _dfs = (word, i, j, matrix, count) => {
   console.log("idx " + count + " " + " letter of current word " + word[count]);
 
   if (word.length === count) return true;
-  if (i < 0 || j < 0 || i >= matrix.length || j >= matrix[0].length || word[i][j] !== word[count]) return false;
+  if (i < 0 || j < 0 || i >= matrix.length || j >= matrix[0].length || matrix[i][j] !== word[count]) return false;
 
   // turn used letters into null so it doesn't count twice
   // how to track used letters once a full word is found?
   let temp = matrix[i][j];
   matrix[i][j] = null;
 
-  let wordFound = dfs(word, i + 1, j, matrix, count + 1)
-    || dfs(word, i - 1, j, matrix, count + 1)
-    || dfs(word, i, j + 1, matrix, count + 1)
-    || dfs(word, i, j - 1, matrix, count + 1);
-
-  matrix[i][j] = temp;
+  let wordFound = _dfs(word, i + 1, j, matrix, count + 1)
+    || _dfs(word, i - 1, j, matrix, count + 1)
+    || _dfs(word, i, j + 1, matrix, count + 1)
+    || _dfs(word, i, j - 1, matrix, count + 1);
 
   return wordFound;
 };
