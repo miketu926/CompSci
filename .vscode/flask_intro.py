0a565e5cfc528d1816fc44fb31f92b1df1707551
miketu <miketu926@gmail.com>
2020-05-09 19:25:40 -0400

    flask + mongodb

---
+++ .vscode/flask_intro.py
@@ -0,0 +1,10 @@
+# use Blueprint from flask to build REST routes
+
+# using mongoengine to connect with flask
+
+import connect from mongoengine
+# takes in mongodb's username, password, host url, etc
+# use blueprint connect to connect blueprint models to the app
+
+# method 2 of connecting to mongodb
+# or use mongoclient() from pymongo to connect
--- .vscode/python_terms.py
+++ .vscode/python_terms.py
@@ -1,3 +1,4 @@
 # decorators - similar to higher order components/functions
 # wraps around another function to provide caching/properties
 @decorator
+
