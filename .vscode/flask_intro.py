5a6a59efb03ec020fd8c645702127f4435b6c8db
miketu <miketu926@gmail.com>
2020-05-12 21:50:01 -0400

    rephrased flask_intro

--- .vscode/flask_intro.py
+++ .vscode/flask_intro.py
@@ -1,17 +1,21 @@
 # use Blueprint from flask to build REST routes
 
-# using mongoengine to connect with flask
+# method 1: using mongoengine to connect with flask
+# method 2: using pymongo to connect
 
-import connect from mongoengine
+# method 1
+from mongoengine import connect
 # takes in mongodb's username, password, host url, etc
 # use blueprint connect to connect blueprint models to the app
 
-# method 2 of connecting to mongodb
-# or use mongoclient() from pymongo to connect
+# method 2
+from pymongo import MongoClient
 
 
 # mongodb basics
 # a DB has many Collections
 # a Collection has many Documents
-# a Document can have EmbeddedDocuments (documents associated with another collection)
+# a Document can have EmbeddedDocuments (documents associated with another model)
+
+# Mongoengine - has a class model structure
 # There are numerous Fields (StringField, IntField, URLField...etc)
